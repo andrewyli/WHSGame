2ea04c0f24536d3e8ac9ccdfdcbf9ec80cc753fc
ddgond <dadangond@gmail.com>
2014-05-25 05:53:38 -0400

    Jocks

--- Jock.py
+++ Jock.py
@@ -1,10 +1,11 @@
 import pygame
+from Player import Player
 from pygame.locals import *
 
 
 class Jock(Player):
-    def __init__(self, image, speed, direction, movespeed):
-        Player.__init__(self, image, speed, direction, movespeed)
+    def __init__(self, image, speed, direction, movespeed, sprint):
+        Player.__init__(self, image, speed, direction, movespeed, sprint)
         
     def update(self, surface):
         event = pygame.event.get()
@@ -12,5 +13,48 @@ class Jock(Player):
         surface.blit(self.image, self.rect)
         if not self.sprint:
             if self.cooldown < 5000:
-                self.cooldown = pygame.time.get_ticks() - self.startCooldown
+                self.cooldown = pygame.time.get_ticks() - self.startCooldown - 5000
+        else:
+            self.cooldown = 5000 - pygame.time.get_ticks() + self.startCooldown
+        if self.cooldown <= 0:
+            self.sprint = False
+            self.image = pygame.image.load("Sprites/JockSprint.png").convert_alpha()
+            self.movespeed = self.movespeed / 2
+            for i in range(self.direction /90):
+                self.image = pygame.transform.rotate(self.image, 90)
             
+    def changeSpeed(self, events):
+        for event in events:
+            if event.type == QUIT:
+                sys.exit()
+            # elif event.type == pygame.KEYDOWN:
+            if event.type == KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.image = pygame.transform.rotate(self.image, -90)
+                    self.direction = (self.direction + 90) % 360
+                if event.key == pygame.K_LEFT:
+                    self.image = pygame.transform.rotate(self.image, 90)
+                    self.direction = (self.direction - 90) % 360
+                if event.key == pygame.K_UP:
+                    if self.direction == 0:
+                        self.speed[0] = 0
+                        self.speed[1] = -self.movespeed
+                    if self.direction == 90:
+                        self.speed[0] = self.movespeed
+                        self.speed[1] = 0
+                    if self.direction == 180:
+                        self.speed[0] = 0
+                        self.speed[1] = self.movespeed
+                    if self.direction == 270:
+                        self.speed[0] = -self.movespeed
+                        self.speed[1] = 0
+                    self.move()
+                if event.key == pygame.K_SPACE:
+                    if self.cooldown == 5000:
+                        self.Sprint = True
+                        self.image = pygame.image.load("Sprites/JockSprint.png").convert_alpha()
+                        self.movespeed = self.movespeed * 2
+                        for i in range(self.direction / 90):
+                            self.image = pygame.transform.rotate(self.image, -90)
+                        self.startCooldown = pygame.time.get_ticks()
+        pygame.event.clear()
